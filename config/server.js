545928417e93a3c554c3115b652e330eebb77fd3
Michal Kadzik <michalkadzik2000@gmail.com>
2020-11-03 20:39:17 +0100

    ssl

--- config/server.js
+++ config/server.js
@@ -1,9 +1,10 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
   admin: {
     auth: {
-      secret: env('ADMIN_JWT_SECRET', 'b26d97e6859c310d7edb7e9d7a80dff7'),
+      secret: env("ADMIN_JWT_SECRET", "b26d97e6859c310d7edb7e9d7a80dff7"),
     },
   },
+  url: "https://gardenmaster-backend.tk/",
 });
